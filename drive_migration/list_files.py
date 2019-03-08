b4fd548e95b70f398d366348db7e9f9a49b4e895
Pieter Lenaerts <pieter@prometheus.localdomain>
2019-03-08 11:00:40 +0100

    First working list files based on quickstart.py

---
+++ drive_migration/list_files.py
@@ -0,0 +1,19 @@
+from g_api_utils import get_api_resource
+
+def main():
+    service = get_api_resource()
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+if __name__ == '__main__':
+    main()
